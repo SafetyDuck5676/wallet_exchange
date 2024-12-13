d4896c40516d45088d915516adf2a1fa131d3bd9
Ducky <dimma5676@gmail.com>
2024-12-13 16:08:05 +0300

    Small updates

--- gw-exchanger/internal/config/default.go
+++ gw-exchanger/internal/config/default.go
@@ -3,6 +3,6 @@ package config
 import "os"
 
 func SetDefaults() {
-	_ = os.Setenv("DATABASE_URL", "postgres://user:password@localhost:5432/exchanger")
+	_ = os.Setenv("DATABASE_URL", "postgres://user:password@localhost:5432/exchanger?sslmode=disable")
 	_ = os.Setenv("GRPC_PORT", "50051")
 }
--- gw-exchanger/internal/storages/postgres/connector.go
+++ gw-exchanger/internal/storages/postgres/connector.go
@@ -12,15 +12,14 @@ type PostgresStorage struct {
 }
 
 func NewPostgresStorage(dsn string) (*PostgresStorage, error) {
+	fmt.Printf("Connecting to database with DSN: %s\n", dsn)
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
-
 	if err := db.Ping(); err != nil {
 		return nil, fmt.Errorf("failed to ping database: %w", err)
 	}
-
 	return &PostgresStorage{db: db}, nil
 }
 
